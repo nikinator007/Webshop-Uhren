957b1f04d294e54619015d482ff2c3ed40aa1556
Sebastian Hietsch <hietschseb@outlook.com>
2017-12-30 22:22:21 +0100

    uhr_1 als Beispiel hinzufügen

--- zusatzdateien/datenbank.js
+++ zusatzdateien/datenbank.js
@@ -4,12 +4,15 @@ datenbank = {
 		preis:"1234",
 		url:"https://example.com",
 		bild:"beispiel.jpg",
+		location:"beispielprodukt.html",
 		kategorie:"armbanduhr"
 	},
 	"uhr_1":{
 		name:"Rolex Cosmograph Daytona",
 		preis:"11.350",
 		url:"https://www.rolex.com/de/watches/cosmograph-daytona/m116500ln-0002.html",
-		bild:"uhr_1.jpg"
+		bild:"uhr_1.jpg",
+		location:"uhr_1.html",
+		kategorie:"armbanduhr"
 	},
 }
