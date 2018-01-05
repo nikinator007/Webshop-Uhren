5c6bd4853ec8aab755c509375233c5dafd391930
Sebastian Hietsch <hietschseb@outlook.com>
2018-01-05 03:31:52 +0100

    Produktseite Uhren von DB abrufen lassen, 4 Uhren zu DB hinzufügen

--- zusatzdateien/datenbank.js
+++ zusatzdateien/datenbank.js
@@ -2,9 +2,9 @@ datenbank = {
 	"uhr_0":{							//Produkt ID
 		hersteller:"BSP.",				//Hersteller
 		name:"Beispielname", 			//Name der Uhr
-		preis:"1234",					//Preis. Idealerweise dem Herstellerpreis anpassen
+		preis:"1.234",					//Preis. Idealerweise dem Herstellerpreis anpassen
 		url:"https://example.com",		//URL der echten Produktseite
-		bild:"beispiel.jpg",			//Vorschaubild der Uhr
+		bild:"uhr_1.jpg",				//Vorschaubild der Uhr
 		dateiname:"beispielprodukt.html",//Dateiname der Uhr auf ClockShop
 		kategorie:"armbanduhr"			//Kategorie
 	},
@@ -15,6 +15,42 @@ datenbank = {
 		url:"https://www.rolex.com/de/watches/cosmograph-daytona/m116500ln-0002.html",
 		bild:"uhr_1.jpg",
 		dateiname:"uhr_1.html",
+		kategorie:"armbanduhr"
+	},
+	"uhr_2":{
+		hersteller:"Breitling",
+		name:"Avenger Blackbird",
+		preis:"4.650",
+		url:"https://www.breitling.com/de/models/avenger/avenger-blackbird/",
+		bild:"uhr_2.jpg",
+		dateiname:"uhr_2.html",
+		kategorie:"armbanduhr"
+	},
+	"uhr_3":{
+		hersteller:"Rolex",
+		name:"Datejust",
+		preis:"8.600",
+		url:"https://www.rolex.com/de/watches/datejust/m126334-0001.html",
+		bild:"uhr_3.jpg",
+		dateiname:"uhr_3.html",
+		kategorie:"armbanduhr"
+	},
+	"uhr_4":{
+		hersteller:"Omega",
+		name:"Speedmaster Moonwatch Meteorite",
+		preis:"12.700",
+		url:"https://zeitenblick.at/omega-speedmaster/",
+		bild:"uhr_4.jpg",
+		dateiname:"uhr_4.html",
+		kategorie:"armbanduhr"
+	},
+	"uhr_5":{
+		hersteller:"Omega",
+		name:"Seamaster Diver 300",
+		preis:"3.475",
+		url:"https://www.uhrinstinkt.de/omega-seamaster-300-m-chrono-diver-212.30.44.50.03.001.html",
+		bild:"uhr_5.jpg",
+		dateiname:"uhr_5.html",
 		kategorie:"armbanduhr"
 	},
 }
